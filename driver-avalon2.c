4d272225c5e29a69294ff74e631484ac9fed0ddc
Luke Dashjr <luke-jr+git@utopios.org>
2014-03-01 05:00:06 +0000

    avalon2: Always use pdiff 1 for shares

--- driver-avalon2.c
+++ driver-avalon2.c
@@ -228,7 +228,7 @@ static int decode_pkg(struct thr_info *thr, struct avalon2_ret *ar, uint8_t *pkg
 				break;
 
 			if (thr && !info->new_stratum)
-				work2d_submit_nonce(thr, &pool->swork, &info->tv_prepared, xnonce2, info->xnonce1, nonce, pool->swork.ntime, NULL, pool->swork.diff / 0.9999847412109375);
+				work2d_submit_nonce(thr, &pool->swork, &info->tv_prepared, xnonce2, info->xnonce1, nonce, pool->swork.ntime, NULL, 1.);
 			break;
 		case AVA2_P_STATUS:
 			memcpy(&tmp, ar->data, 4);
@@ -412,7 +412,8 @@ static int avalon2_stratum_pkgs(int fd, struct pool *pool, struct thr_info *thr)
 	while (avalon2_send_pkg(fd, &pkg, thr) != AVA2_SEND_OK)
 		;
 
-	set_target(target, pool->swork.diff);
+	memset(&target[   0], 0xff, 0x1c);
+	memset(&target[0x1c],    0,    4);
 	memcpy(pkg.data, target, 32);
 	if (opt_debug) {
 		char target_str[(32 * 2) + 1];
